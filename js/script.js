5950d9f8ff8b5fc868c00238afd5a7142564a8e3
Michele Lorusso <kirito102095@hotmail.it>
2021-04-11 12:07:17 +0200

    Burger menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 $(document).ready(function() {
+
+    // Comparsa dei sottomenu
     $('.menu li').mouseenter(function() {
         $(this).children('.dropdown').show();
     });
 
     $('.menu li').mouseleave(function() {
         $(this).children('.dropdown').hide();
-    })
+    });
+
+    // Cambio del font awesome da UP a DOWN
+    $('.leng').mouseenter(function() {
+        $(this).find('.fas').removeClass('fa-chevron-down');
+        $(this).find('.fas').addClass('fa-chevron-up');        
+    });
+
+    $('.leng').mouseleave(function() {
+        $(this).find('.fas').removeClass('fa-chevron-up');
+        $(this).find('.fas').addClass('fa-chevron-down');        
+    });
+
+    // Quando clicco il burger-menu mi compare una lista di link
+    $('.burger-menu').click(function() {
+        var mobileMenu = $('.link');
+
+        if( mobileMenu.hasClass('activate') ) {
+            mobileMenu.removeClass('activate');
+        } else {
+            mobileMenu.addClass('activate')
+        }
+    });
 
 });
